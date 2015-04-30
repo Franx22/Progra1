f5284f5c4599b14094611e2f790312a39ca0d241
fgomez <fgomez@localhost.localdomain>
2015-04-29 22:30:32 -0600

    a

--- EjemploC/src/Tarea1.c
+++ EjemploC/src/Tarea1.c
@@ -4,7 +4,8 @@ struct Distance{
     float inch;
 }d1,d2,sum;
 int main(){
-    printf("Enter information for 1st distance\n");
+	printf("Enter information for 1st distance\n");
+	printf("hola");
     printf("Enter feet: ");
     scanf("%d",&d1.feet);
     printf("Enter inch: ");
